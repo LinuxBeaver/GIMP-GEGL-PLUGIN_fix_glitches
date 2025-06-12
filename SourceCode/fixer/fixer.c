0496791068ecb61fa3d87453ae9c2144734a4b8e
LinuxBeaver <jaydc@gmx.us>
2025-06-12 17:49:45 -0400

    your message here

--- SourceCode/fixer/fixer.c
+++ SourceCode/fixer/fixer.c
@@ -11,7 +11,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with GEGL; if not, see <https://www.gnu.org/licenses/>.
- * Copyright 2006 Øyvind Kolås <pippin@gimp.org>
+ * Credit to Øvind Kolas (pippin) for major GEGL contributions
  * 2025, Beaver Repair GEGL pipeline
 * 
 * graph below
